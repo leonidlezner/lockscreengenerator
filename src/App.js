bc7b4545ba853f4bddd0bf2bc5f848739f625369
Leonid Lezner <kontakt@lezner.de>
2022-10-02 20:12:01 +0200

    Adding headline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Editor from "./components/Editor";
 export default function App() {
   return (
     <div className="container mx-auto sm:mt-3">
+      <div className="text-center">
+        <h1 className="bg-gray-800 px-5 py-2 text-2xl text-gray-500 sm:mb-3 sm:inline-block sm:rounded-md">
+          Lock Screen Generator
+        </h1>
+      </div>
       <Editor />
       <About />
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
 export default function About() {
   return (
-    <div className="mt-2 text-center text-sm text-gray-500">
-      Created by Leonid Lezner. This App is not affiliated, associated,
-      authorized, endorsed by, or in any way officially connected with
-      Volkswagen.
+    <div>
+      <div className="mt-2 text-center text-sm text-gray-500">
+        Created by Leonid Lezner. This App is not affiliated, associated,
+        authorized, endorsed by, or in any way officially connected with
+        Volkswagen.
+      </div>
+      <div className="mt-2 text-center text-sm text-purple-500">
+        <a href="https://github.com/leonidlezner/lockscreengenerator">
+          👨🏻‍💻 Source at GitHub
+        </a>
+      </div>
     </div>
   );
 }
